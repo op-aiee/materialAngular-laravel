d0b18534f4b97803574c5fc350b5c58cf1e0c0e5
clvz <clvz@localhost.localdomain>
2018-02-19 21:25:11 +0100

    initial commit starting boilerplate stuff with git. this commit contains the original forked version of angular material laravel use this as a starting point

---
+++ angular/app/components/list_users/list_users.component.js
@@ -0,0 +1,17 @@
+class ListUsersController{
+    constructor(){
+        'ngInject';
+
+        //
+    }
+
+    $onInit(){
+    }
+}
+
+export const ListUsersComponent = {
+    templateUrl: './views/app/components/list_users/list_users.component.html',
+    controller: ListUsersController,
+    controllerAs: 'vm',
+    bindings: {}
+}
---
+++ tests/angular/app/components/list_users.component.spec.js
@@ -0,0 +1,11 @@
+ngDescribe({
+    name: 'Test list-users component',
+    modules: 'app',
+    element: '<list-users></list-users>',
+    tests: function (deps) {
+
+        it('basic test', () => {
+            //
+        });
+    }
+});
